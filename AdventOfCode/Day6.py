6e438258d96695ec92d03daf0d5370e30be7cb57
quoryl <tatiana.sula@gmail.com>
2020-12-06 12:26:34 +0100

    Day6 part 1

--- AdventOfCode/Day6.py
+++ AdventOfCode/Day6.py
@@ -1,1 +1,12 @@
 import csv
+
+file = 'Day6_data.txt'
+with open(file) as ans:
+    groupAns = ans.read().replace('\r\n', '\n').replace('\n\n', '|').replace('\n', '').split('|')
+
+groupLength = []
+for group in groupAns:
+    s = set((group))
+    groupLength.append(len(s))
+
+print(sum(groupLength))
